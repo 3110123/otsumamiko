275cf119c00e7135824a9601f5fca1fd1862b584
saitoizumi <ilrw3110123@gmail.com>
2021-04-13 15:56:22 +0900

    add: Bookmarkテーブルを作成,アソシエーション追加 #5

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,4 +1,5 @@
-class Snack < ApplicationRecord 
+class Snack < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
   validates :snackname, uniqueness: true, presence: true
   enum alcohol: { beer: 0, wine: 1, sake: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_063514) do
+ActiveRecord::Schema.define(version: 2021_04_13_064946) do
+
+  create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "snack_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["snack_id"], name: "index_bookmarks_on_snack_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "snacks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "snackname"
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 2021_04_13_063514) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "bookmarks", "snacks"
+  add_foreign_key "bookmarks", "users"
 end
