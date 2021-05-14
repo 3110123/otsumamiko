ed9c854855b69ed345ae087a59cd3721c13ba846
saitoizumi <ilrw3110123@gmail.com>
2021-05-14 21:21:59 +0900

    add: 左にドラッグでbookmark解除 #12

--- app/javascript/packs/pages/swipe_bookmark.js
+++ app/javascript/packs/pages/swipe_bookmark.js
@@ -1,16 +1,18 @@
 var card = document.getElementById("exampleModal");
-var modal1 = document.getElementById('showModal');
-var snackId = modal1.dataset.snack
 
 card.ondragstart = function(){
-  onmousedown = function(event){
-    offsetX1 = event.offsetX;
+  var modal1 = document.getElementById('showModal');
+  var snackId = modal1.dataset.snack
+
+  onmousedown = function(e){
+    window.offsetX1 = e.offsetX;
   }
-  onmouseup = function(event){
-    offsetX2 = event.offsetX; 
+  onmouseup = function(e){
+    window.offsetX2 = e.offsetX;
   }
-  if (offsetX2 - offsetX1 > 50){
-    console.log("bookmark");
+
+  var right = window.offsetX2 - window.offsetX1;
+  if ( right > 50){
     $.ajax({
       type: 'POST',
       url: '/snacks/' + snackId + '/bookmarks',
@@ -18,6 +20,23 @@ card.ondragstart = function(){
       headers: {
         'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
       }
-    });
+    }).done(function (results){
+      console.log("create成功");
+    })
+  }else if( right < -150){
+    $.ajax({
+      type: 'DELETE',
+      url: '/snacks/' + snackId + '/bookmarks',
+      dataType: 'json',
+      headers: {
+        'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+      }
+    }).done(function (results){
+      console.log("destroy成功");
+    })
+  }else{
+    console.log("できてなーいよ");
+    return
   }
 }
+
