f86acdbf00fd1bf4ce479662b4fee9270a3cbf86
saitoizumi <ilrw3110123@gmail.com>
2021-05-18 21:40:44 +0900

    add: touchイベント調整 #12

--- app/javascript/packs/pages/swipe_bookmark.js
+++ app/javascript/packs/pages/swipe_bookmark.js
@@ -1,21 +1,69 @@
-var clickEventType = (( window.ontouchstart!==null ) ? 'click':'touchend');
-console.log(clickEventType);
 var card = document.getElementById('showModal');
 var demo = document.getElementById('swipeDemo');
-var bookmarkRange = 20
-
+var mouseBookmarkRange = 20
+var touchBookmarkRange = 300
 
 window.setTimeout(function(){
   demo.style.display = "none";
 }, 5000);
 
 
+// PCmouseイベント
+card.onmousedown = function(e) {
+  window.offsetX1 = e.offsetX
+}
+
+card.ondragend = function(e) {
+  offsetX2 = e.offsetX
+  switchToAction(offsetX2);
+};
+
+function switchToAction(offsetX2) {
+  var snackId = card.dataset.snack
+  if ( offsetX2 > window.offsetX1 + mouseBookmarkRange ){
+    switchToBookmark(snackId)
+  }else if ( offsetX2 < window.offsetX1 - mouseBookmarkRange ){
+    switchToUnbookmark(snackId)
+  }else{
+    return
+  }
+}
+
+// マウス
+function switchToBookmark(snackId) {
+  // 右ドラッグbookmark
+    $.ajax({
+      type: 'POST',
+      url: '/snacks/' + snackId + '/bookmarks',
+      headers: {
+        'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+      }
+    }).done(function() {
+      document.elementFromPoint(100, 100).click();
+      toastr.success('気になっているおつまみに登録しました！');
+    });
+  }
+
+function switchToUnbookmark(snackId) {
+    // 左ドラッグbookmark解除
+    $.ajax({
+      type: 'DELETE',
+      url: '/snacks/' + snackId + '/bookmarks',
+      headers: {
+        'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
+      }
+    }).done(function() {
+      document.elementFromPoint(100, 100).click();
+      toastr.error('気になっているおつまみを解除しました。');
+    });
+}
+
+// スマホtouchイベント
 card.ontouchstart = function(e) {
 	// タッチの情報を含むオブジェクト
 	var touchObject = e.changedTouches[0];
 	// 位置座標を取得する
 	window.touchX1 = touchObject.pageX ;
-  console.log("start:" + window.touchX1);
 }
 
 card.ontouchend = function(e) {
@@ -23,20 +71,16 @@ card.ontouchend = function(e) {
 	var touchObject = e.changedTouches[0] ;
 	// 位置座標を取得する
 	touchX2 = touchObject.pageX ;
-  console.log("end:" + touchX2);
   touchswitchToAction(touchX2);
 }
 
 function touchswitchToAction(touchX2) {
   var snackId = card.dataset.snack
-  if ( touchX2 > window.touchX1 + 20 ){
-    console.log("bookmark");
+  if ( touchX2 > window.touchX1 + touchBookmarkRange ){
     touchswitchToBookmark(snackId)
-  }else if ( touchX2 < window.touchX1 - 20 ){
-    console.log("unbookmark");
+  }else if ( touchX2 < window.touchX1 - touchBookmarkRange ){
     touchswitchToUnbookmark(snackId)
   }else{
-    console.log("残念");
     return
   }
 }
@@ -50,6 +94,7 @@ function touchswitchToBookmark(snackId) {
         'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
       }
     }).done(function() {
+      document.elementFromPoint(100, 100).click();
       toastr.success('気になっているおつまみに登録しました！');
     });
   }
@@ -63,6 +108,7 @@ function touchswitchToUnbookmark(snackId) {
         'X-CSRF-Token' : $('meta[name="csrf-token"]').attr('content')
       }
     }).done(function() {
+      document.elementFromPoint(100, 100).click();
       toastr.error('気になっているおつまみを解除しました。');
     });
 }
