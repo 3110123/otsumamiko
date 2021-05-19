76886665cf3b912845f8dea2abe734eb16eb869a
saitoizumi <ilrw3110123@gmail.com>
2021-05-19 15:35:54 +0900

    add: コントローラーとルーティング追加 #14

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,30 @@
+class PasswordResetsController < ApplicationController
+  def create
+    @user = User.find_by_email(params[:email])
+    @user.deliver_reset_password_instructions! if @user
+    flash[:success] = "メールを送信しました"
+    redirect_to login_path
+  end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+    return not_authenticated if @user.blank?
+  end
+
+  def update
+    @token = params[:id]
+    @user = User,load_from_reset_password_token(params[:id])
+
+    return not_authenticated if @user.blank?
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      flash[:success] = "パスワードを変更しました"
+      redirect_to root_path
+    else
+      flash[:danger] = "パスワードを変更できませんでした"
+      render :edit
+    end
+  end
+end
