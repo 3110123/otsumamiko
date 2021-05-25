03d80af511d05cba09ecf9d5a1d401de4d899246
saitoizumi <ilrw3110123@gmail.com>
2021-05-25 19:29:15 +0900

    add: result画面デザイン #32

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :snack
 
-  validates :comment, presence: true, length: { maximum: 65_535 }
+  validates :comment, presence: true, length: { maximum: 1000 }
   validates :rate, presence: true
 end
