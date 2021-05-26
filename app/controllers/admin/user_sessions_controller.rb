8586297eed36b629f87b2dfb02eb97e89eb99146
saitoizumi <ilrw3110123@gmail.com>
2021-05-26 22:41:48 +0900

    add: adminのログイン画面 #34

---
+++ app/controllers/admin/user_sessions_controller.rb
@@ -0,0 +1,5 @@
+class Admin::UserSessionsController < Admin::BaseController
+  layout 'admin/layouts/admin_login'
+  def new
+  end
+end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = "1.0"
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 Rails.application.config.assets.precompile += ['node_modules/toastr/build/toastr.min.js'] 
-Rails.application .config.assets.precompile += ['node_modules/toastr/build/toastr.min.css']
+Rails.application.config.assets.precompile += ['node_modules/toastr/build/toastr.min.css']
 Rails.application.config.assets.precompile += %w(home.scss)
 
 # Precompile additional assets.
