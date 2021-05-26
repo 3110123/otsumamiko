105a1922969b59a44f1c09445b1e3b2e3bc04480
saitoizumi <ilrw3110123@gmail.com>
2021-05-26 23:00:15 +0900

    add: adminログイン #34

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -2,4 +2,15 @@ class Admin::UserSessionsController < Admin::BaseController
   layout 'admin/layouts/admin_login'
   def new
   end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      flash[:success] = "ログインしました"
+      redirect_to root_path
+    else
+      flash[:error] = "ログインできませんでした"
+      render :new
+    end
+  end
 end
