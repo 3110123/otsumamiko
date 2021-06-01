9b4bb50c86b0c7d9515be8c3884051185d10b93d
saitoizumi <ilrw3110123@gmail.com>
2021-06-01 21:03:25 +0900

    add: 利用規約、パライバシーポリシー #42

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy', to: 'static_pages#privacy'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
