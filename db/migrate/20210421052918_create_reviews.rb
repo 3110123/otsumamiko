6e800b5e139406e8e79a32b2f51c43fc95408b21
saitoizumi <ilrw3110123@gmail.com>
2021-06-01 21:23:01 +0900

    add: inquiryテーブルを作成 #42

--- db/migrate/20210421052918_create_reviews.rb
+++ db/migrate/20210421052918_create_reviews.rb
@@ -4,12 +4,12 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.references :user, null: false, foreign_key: true
       t.references :snack, null: false, foreign_key: true
       t.text :comment, null: false
-      t.integer :rate
-      t.integer :sweetness
-      t.integer :salty
-      t.integer :acidity
-      t.integer :taste
-      t.integer :scent
+      t.integer :rate, null: false
+      t.integer :sweetness, null: false
+      t.integer :salty, null: false
+      t.integer :acidity, null: false
+      t.integer :taste, null: false
+      t.integer :scent, null: false
 
       t.timestamps
     end
