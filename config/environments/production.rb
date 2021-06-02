e5fc4e83ce5e0918d34a58286fabe963da67e0d3
saitoizumi <ilrw3110123@gmail.com>
2021-06-02 14:29:13 +0900

    update: mailer gmailに変更 #42

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,16 +64,15 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = 'otsumamiko.herokuapp.com'
+  host = 'https://otsumamiko.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
+    address: 'smtp.gmail.com',
     port: '587',
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
