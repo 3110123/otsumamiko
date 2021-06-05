8717cd253f6fdb87175bc00a02908b67e9da042e
saitoizumi <ilrw3110123@gmail.com>
2021-06-05 16:50:59 +0900

    add: 一覧loadingアイコン追加 #45

--- app/javascript/packs/pages/page_scroll.js
+++ app/javascript/packs/pages/page_scroll.js
@@ -29,3 +29,21 @@ var loadNextPage = function(){
 window.addEventListener('resize', loadNextPage);
 window.addEventListener('scroll', loadNextPage);
 window.addEventListener('load',   loadNextPage);
+
+
+// loading
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    var loader = $('.loader-wrap');
+
+    //ページの読み込みが完了したらアニメーションを非表示
+    $(window).on('load',function(){
+      loader.fadeOut();
+    });
+
+    //ページの読み込みが完了してなくても2秒後にアニメーションを非表示にする
+    setTimeout(function(){
+      loader.fadeOut();
+    },2000);
+  });
+});
