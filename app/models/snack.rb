c9579ba9ed4a8f6074e9b192e439eb0f2f11998a
saitoizumi <ilrw3110123@gmail.com>
2021-06-10 23:13:36 +0900

    add: タグ検索 #47

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -6,4 +6,11 @@ class Snack < ApplicationRecord
   has_one_attached :image
   validates :name, uniqueness: true, presence: true
   enum alcohol: { beer: 0, wine: 1, sake: 2 }
+
+  scope :has_tag_id_all, ->tag_ids { includes(:tag_relationships).where(tag_relationships: {tag_id: tag_ids}).group(ids).having('count(snack_id) = ?', count(tag_ids))
+   }
+
+  def self.ransackable_scopes(auth_object = nil)
+    %i(has_tag_id_all)
+  end
 end
