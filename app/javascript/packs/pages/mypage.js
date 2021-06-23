a63958203ab781d992ba9d3a2083908401fd409f
saitoizumi <ilrw3110123@gmail.com>
2021-06-23 22:19:09 +0900

    add: mypageにlozad.jsを追加 #56

--- app/javascript/packs/pages/mypage.js
+++ app/javascript/packs/pages/mypage.js
@@ -1,4 +1,7 @@
 var loadNextPage = function(){
+  const observer = lozad();
+  observer.observe();
+
   if ($('#next_link').data("loading")){ return }
   var wBottom  = $(window).scrollTop() + $(window).height();
   var elBottom = $('#records_table').offset().top + $('#records_table').height();
