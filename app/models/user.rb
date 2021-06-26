96c086dad2bc361eb775c139d4d000c4b56c9880
saitoizumi <ilrw3110123@gmail.com>
2021-06-27 02:51:18 +0900

    update: password length変更 #28

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :reviews_snacks, through: :reviews, source: :snack
   authenticates_with_sorcery!
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :name, presence: true, length: { maximum: 255 }
