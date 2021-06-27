2e30175de1737ccb1d10e28d4bf30a5c33e7600f
saitoizumi <ilrw3110123@gmail.com>
2021-06-27 16:11:04 +0900

    add: tag model spec #64

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :tag_relationships, dependent: :destroy
   has_many :snacks, through: :tag_relationships
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: { case_sensitive:  true }, presence: true
 end
