3f87c3add94fb947a4bca2a4a2c8da1a13ba6a16
saitoizumi <ilrw3110123@gmail.com>
2021-06-29 16:07:55 +0900

    update: rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,26 +16,26 @@ module ApplicationHelper
       canonical: request.original_url,
       icon: [
         { href: image_url('favicon.ico') },
-        { href: image_url('ogp_apple_touch_icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+        { href: image_url('ogp_apple_touch_icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
       ],
       og: {
         site_name: :site,
         title: :title,
-        description: :description, 
+        description: :description,
         type: 'website',
         url: request.original_url,
         image: image_url('ogp_twitter_card.png'),
-        locale: 'ja_JP',
+        locale: 'ja_JP'
       },
       twitter: {
         site: '@st3110123',
         card: 'summary_large_image',
-        image: image_url('ogp_twitter_card.png'),
+        image: image_url('ogp_twitter_card.png')
       }
     }
   end
 
-  def lazy_image_tag(source, options={})
+  def lazy_image_tag(source, options = {})
     options['data-src'] = asset_path(source)
     if options[:class].blank?
       options[:class] = 'lozad'
--- app/models/snack.rb
+++ app/models/snack.rb
@@ -4,11 +4,9 @@ class Snack < ApplicationRecord
   has_many :tags, through: :tag_relationships
   has_many :reviews, dependent: :destroy
   has_one_attached :image
-  validates :name, uniqueness: { case_sensitive:  true }, presence: true
+  validates :name, uniqueness: { case_sensitive: true }, presence: true
   validates :alcohol, presence: true
   enum alcohol: { beer: 0, wine: 1, sake: 2 }
 
-  scope :name_contain, -> (name) { where('snacks.name LIKE ?', "%#{name}%") }
-
- 
+  scope :name_contain, ->(name) { where('snacks.name LIKE ?', "%#{name}%") }
 end
