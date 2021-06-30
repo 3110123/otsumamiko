11c17f11b35746d042e75bb7fb1f238266cc29f6
saitoizumi <ilrw3110123@gmail.com>
2021-07-01 01:49:26 +0900

    update: console.log削除

--- app/javascript/packs/pages/header.js
+++ app/javascript/packs/pages/header.js
@@ -2,7 +2,6 @@ var navToggle = document.getElementById('nav-toggle');
 if(navToggle){
   navToggle.addEventListener('click', function(){
     var body = document.getElementsByTagName('body')[0];
-    console.log(body);
     body.classList.toggle('open');
   }, false);
 }
