8c341cb7dc762338761a9568887b05d4af52c3cd
saitoizumi <ilrw3110123@gmail.com>
2021-07-01 01:35:50 +0900

    update: headerのjqueryをjsに書き換え

--- app/javascript/packs/pages/header.js
+++ app/javascript/packs/pages/header.js
@@ -1,7 +1,8 @@
-$(document).on('turbolinks:load', function() {
-  $(function () {
-    $('#nav-toggle').on('click', function() {
-      $('body').toggleClass('open');
-    });
-  });
-});
+var navToggle = document.getElementById('nav-toggle');
+if(navToggle){
+  navToggle.addEventListener('click', function(){
+    var body = document.getElementsByTagName('body')[0];
+    console.log(body);
+    body.classList.toggle('open');
+  }, false);
+}
